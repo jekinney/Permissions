05cb2c45080b1e44c9eb0b4564b3ec543fbb6ea9
jekinney <jekinneys@yahoo.com>
2015-06-09 13:43:45 -0500

    Added Route as naming convention

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::group(['prefix'=>'admin'], function() {
-    get('/', ['as'=>'admin.home', 'uses'=>'Backend\ManageUserController@all']);
-    get('users', ['as' => 'admin.user.all', 'uses' => 'Backend\ManageUserController@all']);
-    get('user/@{username}/editor', ['as' => 'admin.user.editor', 'uses' => 'Backend\ManageUserController@edit']);
-    put('user/update', ['as' => 'admin.user.update', 'uses' => 'Backend\ManageUserController@update']);
-    get('groups', ['as' => 'admin.group.all', 'uses' => 'Backend\GroupController@all']);
-    put('group/{id}/update', ['as' => 'admin.group.update', 'uses' => 'Backend\GroupController@update']);
-    post('group/store', ['as' => 'admin.group.store', 'uses' => 'Backend\GroupController@store']);
-    delete('group/delete/{id}', ['as' => 'admin.group.delete', 'uses' => 'Backend\GroupController@delete']);
+Route::group(['prefix'=>'admin', 'as'=>'admin.'], function() {
+    get('/', ['as'=>'home', 'uses'=>'Backend\ManageUserController@all']);
+    get('users', ['as' => 'user.all', 'uses' => 'Backend\ManageUserController@all']);
+    get('user/@{username}/editor', ['as' => 'user.editor', 'uses' => 'Backend\ManageUserController@edit']);
+    put('user/update', ['as' => 'user.update', 'uses' => 'Backend\ManageUserController@update']);
+    get('groups', ['as' => 'group.all', 'uses' => 'Backend\GroupController@all']);
+    put('group/{id}/update', ['as' => 'group.update', 'uses' => 'Backend\GroupController@update']);
+    post('group/store', ['as' => 'group.store', 'uses' => 'Backend\GroupController@store']);
+    delete('group/delete/{id}', ['as' => 'group.delete', 'uses' => 'Backend\GroupController@delete']);
 });
